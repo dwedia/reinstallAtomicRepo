5dae9453761eccc4c6f8d1dc1ad0cc7f60f40b1d
dwedia <eric@bellaiche.net>
2024-07-06 23:47:22 +0200

    wip - i3 config

--- i3Install.sh
+++ i3Install.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 # Install i3 and packages
-rpm-ostree install -y --apply-live i3 lxpolkit
+rpm-ostree install -y --apply-live i3 lxpolkit volumeicon feh rofi rofi-themes
 
 # Run ansible playbook to set i3 settings.
 ansible-playbook i3Install-playbook.yml -k
