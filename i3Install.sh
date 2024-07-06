518c2225c7445f12e78fb0466c01aeec37cea446
dwedia <eric@bellaiche.net>
2024-07-06 21:59:56 +0200

    changes

---
+++ i3Install.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+
+# Install i3 and packages
+rpm-ostree install -y --apply-live i3
+
+# Run ansible playbook to set i3 settings.
+ansible-playbook i3Install-playbook.yml -k
---
+++ swayInstall.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+
+# Install sway and packages
+rpm ostree install -y --apply-live sway swaylock swayidle swaybg rofi-wayland rofi-themes
