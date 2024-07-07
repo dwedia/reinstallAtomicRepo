95c32bebe8aeda71ab11a05c4d048d99ac63a279
dwedia <eric@bellaiche.net>
2024-07-07 20:36:21 +0200

    changed -k to -K

--- i3Install.sh
+++ i3Install.sh
@@ -4,4 +4,4 @@
 rpm-ostree install -y --apply-live i3 lxpolkit volumeicon nitrogen rofi rofi-themes
 
 # Run ansible playbook to set i3 settings.
-ansible-playbook i3Install-playbook.yml -k
+ansible-playbook i3Install-playbook.yml -K
