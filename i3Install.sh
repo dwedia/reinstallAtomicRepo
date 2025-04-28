474935c20c5221ebf7b058d4c9a60b37769c26e2
eribel <eribel@rm.dk>
2025-04-28 13:20:57 +0200

    added gnome-session-xsession to i3install script

--- i3Install.sh
+++ i3Install.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 # Install i3 and packages
-rpm-ostree install --idempotent -y --apply-live i3 lxpolkit volumeicon nitrogen rofi rofi-themes
+rpm-ostree install --idempotent -y --apply-live i3 lxpolkit volumeicon nitrogen rofi rofi-themes gnome-session-xsession
 
 # Run ansible playbook to set i3 settings.
 ansible-playbook i3Install-playbook.yml -K
